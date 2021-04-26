a668ccaab6e3f7f3f2a356323e1b39b30c759d01
Tomasz Kapischke <tomasz.kapischke@gmail.com>
2021-04-26 21:31:49 +0200

    Add Swagger

--- WebApi.Samples/Startup.cs
+++ WebApi.Samples/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebApi.Samples.Infrastructure;
 
 namespace WebApi.Samples
@@ -18,6 +19,16 @@ namespace WebApi.Samples
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructure();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\WebApi.Samples.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WebApi.Samples",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,10 +40,15 @@ namespace WebApi.Samples
             }
 
             app.UseRouting();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi.Samples");
+            });
+ 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
